79f42621ca81df872c3f835100301a492b24fbfa
王新雷 <173311763@qq.com>
2018-03-28 17:53:48 +0800

    添加时间

--- pages/addToMeet/add.js
+++ pages/addToMeet/add.js
@@ -1,118 +1,87 @@
+const days = []
+const hours = []
+const minutes = ["00", "15", "30", "45"]
+const weekList = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+for (let i = 0; i < 7; i++) {
+  var date = new Date()
+  date.setDate(date.getDate() + i)
+  var day = date.getDate()
+  var items = date.getMonth() + 1 + '月' + (day > 9 ? day : '0' + day) + '日 ' + weekList[date.getDay()]
+  days.push(items)
+}
+
+for (let i = 1; i <= 23; i++) {
+  if(i > 9) {
+    hours.push(i)
+  } else {
+    hours.push('0' + i)
+  }
+}
+function checkHourOrMinute(hour, minute) {
+  var h, m
+  if(minute > 0 && minute < 15) {
+    m = 1
+    h = hour
+  } else if(minute > 15 && minute < 30) {
+    m = 2
+    h = hour
+  } else if(minute > 30 && minute < 45) {
+    m = 3
+    h = hour
+  } else if(minute > 45) {
+    m = 0
+    if( hour == 23) {
+      h = 0
+    } else {
+      h = hour
+    }
+  }
+  return {
+    hour: h,
+    minute: m
+  }
+}
+
+// 当前显示的时间
+const nowDate = new Date()
+const tempDate = nowDate.getDate()
+// 当前日期
+const nowDay = nowDate.getMonth() + 1 + '月' + (tempDate > 9 ? tempDate : '0' + tempDate) + '日 ' + weekList[nowDate.getDay()]
+const hm = checkHourOrMinute(nowDate.getHours(), nowDate.getMinutes())
+const value = [0, hm.hour, hm.minute]
+
 Page({
   data: {
     index: 0,
-    multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'], ['猪肉绦虫', '吸血虫']],
-    objectMultiArray: [
-      [
-        {
-          id: 0,
-          name: '无脊柱动物'
-        },
-        {
-          id: 1,
-          name: '脊柱动物'
-        }
-      ], [
-        {
-          id: 0,
-          name: '扁性动物'
-        },
-        {
-          id: 1,
-          name: '线形动物'
-        },
-        {
-          id: 2,
-          name: '环节动物'
-        },
-        {
-          id: 3,
-          name: '软体动物'
-        },
-        {
-          id: 3,
-          name: '节肢动物'
-        }
-      ], [
-        {
-          id: 0,
-          name: '猪肉绦虫'
-        },
-        {
-          id: 1,
-          name: '吸血虫'
-        }
-      ]
-    ],
-    multiIndex: [0, 0, 0]
+    days: days,
+    day: nowDay,
+    hours: hours,
+    hour: hm.hour,
+    minutes: minutes,
+    minute: minutes[hm.minute],
+    value: value,
+    timeIsShow: false
   },
-  bindMultiPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      multiIndex: e.detail.value
-    })
-  },
-  bindMultiPickerColumnChange: function (e) {
-    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
-    var data = {
-      multiArray: this.data.multiArray,
-      multiIndex: this.data.multiIndex
-    };
-    data.multiIndex[e.detail.column] = e.detail.value;
-    switch (e.detail.column) {
-      case 0:
-        switch (data.multiIndex[0]) {
-          case 0:
-            data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
-            data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
-            break;
-          case 1:
-            data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
-            data.multiArray[2] = ['鲫鱼', '带鱼'];
-            break;
-        }
-        data.multiIndex[1] = 0;
-        data.multiIndex[2] = 0;
-        break;
-      case 1:
-        switch (data.multiIndex[0]) {
-          case 0:
-            switch (data.multiIndex[1]) {
-              case 0:
-                data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
-                break;
-              case 1:
-                data.multiArray[2] = ['蛔虫'];
-                break;
-              case 2:
-                data.multiArray[2] = ['蚂蚁', '蚂蟥'];
-                break;
-              case 3:
-                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
-                break;
-              case 4:
-                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
-                break;
-            }
-            break;
-          case 1:
-            switch (data.multiIndex[1]) {
-              case 0:
-                data.multiArray[2] = ['鲫鱼', '带鱼'];
-                break;
-              case 1:
-                data.multiArray[2] = ['青蛙', '娃娃鱼'];
-                break;
-              case 2:
-                data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
-                break;
-            }
-            break;
-        }
-        data.multiIndex[2] = 0;
-        console.log(data.multiIndex);
-        break;
+  // 修改时间
+  changeTime: function (e) {
+    if(this.data.timeIsShow) {
+      this.setData({
+        timeIsShow: false
+      })
+    } else {
+      this.setData({
+        timeIsShow: true
+      })
     }
-    this.setData(data);
+  },
+  // 时间切换时间
+  bindChange: function (e) {
+    const val = e.detail.value
+    this.setData({
+      day: this.data.days[val[0]],
+      hour: this.data.hours[val[1]],
+      minute: this.data.minutes[val[2]]
+    })
   }
+  
 })
